6c94891d9d7db81ad9b2b235562db62bd93364c4
Ninja <phaser.84059@gmail.com>
2021-02-17 10:40:38 +0530

    Added proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from '@apollo/react-hooks'
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://whispering-castle-21410.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
