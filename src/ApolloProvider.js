7817ce35f44e65c522840c961e67bb67bf3314a1
Saket Kumar Ray <phaser.84059@gmail.com>
2022-06-19 11:49:17 +0530

    Add proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import App from './App'
-import ApolloClient from 'apollo-client'
-import {InMemoryCache} from 'apollo-cache-inmemory'
-import {createHttpLink} from 'apollo-link-http'
-import {ApolloProvider} from '@apollo/react-hooks'
-import { setContext } from 'apollo-link-context';
+import React from "react";
+import App from "./App";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-    uri: 'https://whispering-castle-21410.herokuapp.com/'
+  uri: "https://infinite-ocean-06606.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
-    const token = localStorage.getItem('jwtToken');
-    return{
-        headers: {
-            Authorization: token ? `Bearer ${token}` : ``
-        }
-    }
-})
-
-const client = new ApolloClient({
-    link:authLink.concat(httpLink),
-    cache: new InMemoryCache()
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : ``,
+    },
+  };
 });
 
-export default(
-    <ApolloProvider client={client}>
-        <App/>
-    </ApolloProvider>
-)
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
