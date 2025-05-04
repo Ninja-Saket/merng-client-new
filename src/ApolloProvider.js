5bfae828c19af6a5659e3a60b4e1e4739fd0a7f8
Saket Ray <phaser.84059@gmail.com>
2025-05-05 00:15:27 +0530

    updated backend url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://infinite-ocean-06606.herokuapp.com/",
+  uri: "http://64.227.169.105:5000/",
 });
 
 const authLink = setContext(() => {
