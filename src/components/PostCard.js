3be70f72521163d58571240b9112246c99107f8c
Saket Ray <phaser.84059@gmail.com>
2025-05-05 00:46:01 +0530

    updated postcard typo for time

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -20,7 +20,7 @@ function PostCard ({post: {body, createdAt, id, username,likeCount, commentCount
                 src='https://react.semantic-ui.com/images/avatar/large/molly.png'
                 />
                 <Card.Header>{username}</Card.Header>
-                <Card.Meta as={Link} to={`/posts/${id}`}>j{moment(createdAt).fromNow(true)}</Card.Meta>
+                <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
                 <Card.Description>{body}</Card.Description>
             </Card.Content>
             
